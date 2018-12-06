51593b4a815c7060446d05919a7a3fd76bde5fd8
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-06 11:30:56 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,4 +16,27 @@ client.on("guildMemberAdd", member => {
 **`) 
 }).catch(console.error)
 })
+
+client.on('guildMemberAdd', member => {
+const mahmoud= member.guild.channels.get("512674548382957573");
+if(!mahmoud) return;
+if(mahmoud) {
+setTimeout(() => mahmoud.send(`**# Welllcome , to Mal Shop  🌹 .**`), 4000)        
+}
+});
+
+client.on('guildMemberAdd', member => {
+const mahmoud= member.guild.channels.get("513396697406373898");
+if(!mahmoud) return;
+if(mahmoud) {
+setTimeout(() => mahmoud.send(`**# Welllcome , to Maliciouns™  🌹 .**`), 4000)        
+}
+});
+
+
+
+
+
+
+
 client.login('process.env.BOT_TOKEN');
